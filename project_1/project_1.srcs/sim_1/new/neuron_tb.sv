3c0825bc08ba9072408da0e38191067a766dffb7
Alex Gu <gua@mit.edu>
2020-11-09 00:21:42 -0500

    added first draft of full neural network and testbench

--- project_1/project_1.srcs/sim_1/new/neuron_tb.sv
+++ project_1/project_1.srcs/sim_1/new/neuron_tb.sv
@@ -7,18 +7,24 @@ module neuron_tb;
     
     logic clk;
     logic rst;
+    logic ready;
     logic [1:0] length;
     logic [WIDTH-1:0] weights[1:0];
+    logic [WIDTH-1:0] bias;
     logic [WIDTH-1:0] previous_neurons [1:0];
     logic [WIDTH-1:0] neuron_output;
+    logic done;
     
     neuron #(.WIDTH(WIDTH), .DECIMALS(DECIMALS)) uut (
         .clk(clk),
         .rst(rst),
+        .ready(ready),
         .length(length),
         .weights(weights),
+        .bias(bias),
         .previous_neurons(previous_neurons),
-        .neuron_output(neuron_output)         
+        .neuron_output(neuron_output),
+        .done(done)    
     );
     
     always begin
@@ -30,14 +36,17 @@ module neuron_tb;
         clk = 0;
         rst = 0;
         length = 2;
+        bias = 8'b0;
         weights[1] = 8'b0_0010_000;
         weights[0] = 8'b0_0010_000;
         previous_neurons[1] = 8'b0_0010_000;
         previous_neurons[0] = 8'b0_0101_000;
         #10;
         rst = 1;
+        ready = 1;
         #10;
         rst = 0;
+        ready = 0;
         #10;
         #100;
         assert (neuron_output == 8'b0_1110_000) else $error("fc network failed to multiply :(");
--- project_1/project_1.srcs/sources_1/new/neuron.sv
+++ project_1/project_1.srcs/sources_1/new/neuron.sv
@@ -3,10 +3,13 @@
 module neuron #(parameter WIDTH = 8, DECIMALS = 3) (
     input clk,
     input rst,
-    input logic [1:0] length,
+    input logic ready,
+    input logic [1:0] length, // shape of the weight matrix
     input logic [WIDTH-1:0] weights[1:0], // w[i], TODO(parametrize)
+    input logic [WIDTH-1:0] bias, // b
     input logic [WIDTH-1:0] previous_neurons [1:0], // x[i], TODO(parametrize)
-    output logic [WIDTH-1:0] neuron_output // sum of w[i] x[i]
+    output logic done,
+    output logic [WIDTH-1:0] neuron_output // sum of w[i] x[i] + b
 );
     
     logic [WIDTH-1:0] w_i;
@@ -20,8 +23,9 @@ module neuron #(parameter WIDTH = 8, DECIMALS = 3) (
     
     logic [1:0] i; // TODO: parametrize
     always_ff @(posedge clk) begin
-        if (rst) begin 
-            neuron_output <= 0; 
+        if (rst || ready) begin 
+            neuron_output <= bias; // start at bias, and add weights one by one
+            done <= 0;
             i <= 1;
             w_i <= weights[0];
             x_i <= previous_neurons[0];
@@ -32,9 +36,13 @@ module neuron #(parameter WIDTH = 8, DECIMALS = 3) (
             neuron_output <= neuron_output + product_i;
             i <= i+1;
         end
-        else if (i == length) begin
+        else if (i == length) begin // do the final computation and assert done
             neuron_output <= neuron_output + product_i;
             i <= i+1;
+            done <= 1;
+        end
+        else if (i == length + 1) begin // set done back to 0
+            done <= 0;
         end
     end
 
